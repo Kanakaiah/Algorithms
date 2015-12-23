afbf0618a5b4b121808eacc8fa8c8e26f125fb58
Kanakaiah <kanakaiah.e@gmail.com>
2015-12-22 18:29:29 -0800

    Enumarable and Enumerator

---
+++ Algrthm/LanguageFeatures/Enumerator/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algrthm.LanguageFeatures.Enumerator
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Student(int Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
+}
---
+++ Algrthm/LanguageFeatures/Enumerator/StudentCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algrthm.LanguageFeatures.Enumerator
+{
+    public class StudentCollection : IEnumerator, IEnumerable
+    {
+        ArrayList StudList = new ArrayList();
+
+        private int Position = -1;
+
+        public void AddStudent(Student std)
+        {
+            StudList.Add(std);
+        }
+
+
+        public object Current
+        {
+            get
+            {
+                return StudList[Position];
+            }
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            if (Position < StudList.Count - 1)
+            {
+                ++Position;
+                return true;
+            }
+            return false;
+        }
+
+
+
+        public void Reset()
+        {
+            Position = -1;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this;
+
+        }
+    }
+}

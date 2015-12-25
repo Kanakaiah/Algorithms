d1dcd766f1b6ba0710e4c7e34dc190068292db60
Kanakaiah Etipakam <kanakaiah.e@gmail.com>
2015-12-25 01:55:54 -0800

    Words in reverse print including spaces

---
+++ Algrthm.Test/Interviews/MS/WordReverse.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algrthm.Test.Interviews.MS
+{
+    [TestClass]
+    public class WordReverse
+    {
+
+        [TestMethod]
+        public void TestResult()
+        {
+            Algrthm.Interviews.MS.WordReverse wordReverse = new Algrthm.Interviews.MS.WordReverse();
+
+            System.Diagnostics.Debug.WriteLine(wordReverse.GetResult("Hello How are you"));
+            System.Diagnostics.Debug.WriteLine(wordReverse.GetResult(" Hello How are you"));
+            System.Diagnostics.Debug.WriteLine(wordReverse.GetResult("Hello  How are you"));
+            System.Diagnostics.Debug.WriteLine(wordReverse.GetResult(" Hello How   are you  "));
+            
+        }
+    }
+}
---
+++ Algrthm/Interviews/MS/WordReverse.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algrthm.Interviews.MS
+{
+    
+    public class WordReverse
+    {
+        public string GetResult(string str)
+        {
+            int startIndex = 0;
+            string result = string.Empty;
+
+            for (int i = 0; i <str.Length-1; i++)
+            {
+                if(str[i]==' ')
+                {
+
+                    if (startIndex == 0)
+                    {
+                        if (i == 0)
+                        {
+                            result = " ";
+                            startIndex += 1;
+                        }
+                        else
+                        {
+                            result = " " + str.Substring(startIndex, i);
+                            startIndex = i + 1;
+                        }
+                    }
+                    else
+                    {
+
+                        if (startIndex == i)
+                        {
+                            result = " " + result;
+                            startIndex += 1;
+                        }
+                        else
+                        {
+                            result = " " + str.Substring(startIndex, i-startIndex)+ result;
+                            startIndex =i+1;
+                        }
+
+                    }
+                }
+            }
+
+            if(str.Length!=result.Length)
+            result = str.Substring(startIndex, str.Length - startIndex) + result;
+
+
+            return result;
+
+        }
+
+    }
+}
